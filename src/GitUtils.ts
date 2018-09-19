a72a659d007d21074a5e286d5600a7ef8b6363d4
carney520 <carney520@hotmail.com>
2018-09-19 09:12:24 +0800

    add prettier; 添加tag操作方法；优化deploy tags更新流程

--- src/GitUtils.ts
+++ src/GitUtils.ts
@@ -1,24 +1,24 @@
 /**
  * git 相关的帮助方法
  */
-import cp from 'child_process';
-import url from 'url';
-import path from 'path';
-import fs from 'fs-extra';
-import Log from './Log';
+import cp from 'child_process'
+import url from 'url'
+import path from 'path'
+import fs from 'fs-extra'
+import Log from './Log'
 
-const StatusRegexp = /^([ADM? ])([ADM? ])\s(.+)$/;
+const StatusRegexp = /^([ADM? ])([ADM? ])\s(.+)$/
 
 export interface BranchDesc {
-  name: string;
-  ref: string;
-  remote: boolean;
+  name: string
+  ref: string
+  remote: boolean
 }
 
 export interface StatusDesc {
-  file: string;
-  stagedStatus: FileStatus;
-  unstagedStatus: FileStatus;
+  file: string
+  stagedStatus: FileStatus
+  unstagedStatus: FileStatus
 }
 
 export enum FileStatus {
@@ -35,25 +35,25 @@ export const FileStatusMap: { [key: string]: FileStatus } = {
   '?': FileStatus.UNTRACKED,
   ' ': FileStatus.NONE,
   A: FileStatus.ADDED,
-};
+}
 
 export default class GitUtils {
-  public workDir: string;
-  public repoDir: string;
-  public remote: string;
-  public remoteName: string;
-  public basename: string;
-  public verbose: boolean = false;
-  public focusedBranch: string = 'master';
-  public Logger = Log;
+  public workDir: string
+  public repoDir: string
+  public remote: string
+  public remoteName: string
+  public basename: string
+  public verbose: boolean = false
+  public focusedBranch: string = 'master'
+  public Logger = Log
 
   public constructor(workDir: string, repoDir: string, remote: string, remoteName: string = 'origin') {
-    this.workDir = workDir;
-    this.repoDir = repoDir;
-    this.remote = remote;
-    this.remoteName = remoteName;
-    const pathname = url.parse(remote).pathname || '/repo';
-    this.basename = path.basename(pathname, '.git');
+    this.workDir = workDir
+    this.repoDir = repoDir
+    this.remote = remote
+    this.remoteName = remoteName
+    const pathname = url.parse(remote).pathname || '/repo'
+    this.basename = path.basename(pathname, '.git')
   }
 
   /**
@@ -62,12 +62,12 @@ export default class GitUtils {
   public initial() {
     if (!fs.existsSync(this.repoDir)) {
       // 仓库不存在
-      const cmd = `git clone ${this.remote} ${this.basename}`;
-      this.Logger.log(cmd);
-      cp.execSync(cmd, { cwd: this.workDir });
+      const cmd = `git clone ${this.remote} ${this.basename}`
+      this.Logger.log(cmd)
+      cp.execSync(cmd, { cwd: this.workDir })
     } else {
       // 更新状态
-      this.updateBranch();
+      this.updateBranch()
     }
   }
 
@@ -75,140 +75,176 @@ export default class GitUtils {
    * 初始化分支
    */
   public initialBranch(branchName: string = 'master') {
-    const locals = this.getLocalBranches();
+    const locals = this.getLocalBranches()
     if (locals.findIndex(i => i.name === branchName) === -1) {
-      const remotes = this.getRemoteBranches();
+      const remotes = this.getRemoteBranches()
       if (remotes.findIndex(i => i.name === branchName) === -1) {
         // 本地和远程都不存在, 创建分支
-        const masterCommit = this.getMasterCommit();
-        this.createBranch(branchName, masterCommit);
+        const masterCommit = this.getMasterCommit()
+        this.createBranch(branchName, masterCommit)
       }
     }
 
     // created
-    this.checkout(branchName);
-    this.focusedBranch = branchName;
+    this.checkout(branchName)
+    this.focusedBranch = branchName
   }
 
   /**
    * 更新远程状态
    */
   public updateBranch() {
-    const cmd = `git pull -t --ff ${this.remoteName} master`;
-    this.Logger.log(cmd);
-    cp.execSync(cmd, { cwd: this.repoDir });
+    const cmd = `git pull -t --ff ${this.remoteName} master`
+    this.Logger.log(cmd)
+    cp.execSync(cmd, { cwd: this.repoDir })
   }
 
   public createBranch(name: string, commit: string) {
-    const cmd = `git checkout -b ${name} ${commit}`;
-    this.Logger.log(cmd);
-    cp.execSync(cmd, { cwd: this.repoDir });
+    const cmd = `git checkout -b ${name} ${commit}`
+    this.Logger.log(cmd)
+    cp.execSync(cmd, { cwd: this.repoDir })
   }
 
   public checkout(ref: string) {
-    const cmd = `git checkout ${ref}`;
-    this.Logger.log(cmd);
-    cp.execSync(cmd, { cwd: this.repoDir });
+    const cmd = `git checkout ${ref}`
+    this.Logger.log(cmd)
+    cp.execSync(cmd, { cwd: this.repoDir })
   }
 
   /**
    * 获取master分支的commit sha
    */
   public getMasterCommit() {
-    const cmd = `git rev-parse master`;
-    this.Logger.log(cmd);
-    return cp.execSync(cmd, { cwd: this.repoDir }).toString();
+    const cmd = `git rev-parse master`
+    this.Logger.log(cmd)
+    return cp.execSync(cmd, { cwd: this.repoDir }).toString()
   }
 
   public getBranchCommit(branch: string) {
-    const cmd = `git rev-parse ${branch}`;
-    this.Logger.log(cmd);
-    return cp.execSync(cmd, { cwd: this.repoDir }).toString();
+    const cmd = `git rev-parse ${branch}`
+    this.Logger.log(cmd)
+    return cp.execSync(cmd, { cwd: this.repoDir }).toString()
   }
 
   /**
    * 获取所有分支，需要在调用前pull一下
    */
   public getAllBranches() {
-    return this.getLocalBranches().concat(this.getRemoteBranches());
+    return this.getLocalBranches().concat(this.getRemoteBranches())
   }
 
   public getLocalBranches() {
-    const refDir = path.join(this.repoDir, '.git/refs/heads');
+    const refDir = path.join(this.repoDir, '.git/refs/heads')
     return this.getFiles(refDir).map(item => ({
       name: item.name,
       ref: item.content,
       remote: false,
-    }));
+    }))
   }
 
   public getRemoteBranches() {
-    const refDir = path.join(this.repoDir, `.git/refs/remotes/${this.remoteName}`);
+    const refDir = path.join(this.repoDir, `.git/refs/remotes/${this.remoteName}`)
 
     return this.getFiles(refDir)
       .filter(item => {
-        return ['HEAD'].indexOf(item.name) === -1;
+        return ['HEAD'].indexOf(item.name) === -1
       })
       .map(item => ({
         name: item.name,
         ref: item.content,
         remote: true,
-      }));
+      }))
   }
 
   public commit(message: string) {
-    console.log(message);
-    const cmd = `git commit -n -m "${message}"`;
-    this.Logger.log(cmd);
-    cp.execSync(cmd, { cwd: this.repoDir });
+    const cmd = `git commit -n -m "${message}"`
+    this.Logger.log(cmd)
+    cp.execSync(cmd, { cwd: this.repoDir })
   }
 
   public shouldCommit() {
-    return this.status().length !== 0;
+    return this.status().length !== 0
   }
 
   public status(): StatusDesc[] {
-    const cmd = `git status -s`;
-    this.Logger.log(cmd);
-    const res = cp.execSync(cmd, { cwd: this.repoDir }).toString();
+    const cmd = `git status -s`
+    this.Logger.log(cmd)
+    const res = cp.execSync(cmd, { cwd: this.repoDir }).toString()
     return res
       .split('\n')
       .filter(i => !!i)
       .map(str => {
-        const matched = str.match(StatusRegexp);
+        const matched = str.match(StatusRegexp)
         if (!matched) {
-          return null;
+          return null
         }
-        const [stagedStatus, unstagedStatus, file] = matched.slice(1);
+        const [stagedStatus, unstagedStatus, file] = matched.slice(1)
         return {
           file,
           stagedStatus: this.mapStatusDesc(stagedStatus),
           unstagedStatus: this.mapStatusDesc(unstagedStatus),
-        } as StatusDesc;
+        } as StatusDesc
       })
-      .filter(i => !!i) as StatusDesc[];
+      .filter(i => !!i) as StatusDesc[]
   }
 
-  public push() {
-    const cmd = `git push --tags ${this.remoteName} ${this.focusedBranch}`;
-    this.Logger.log(cmd);
-    cp.execSync(cmd, { cwd: this.repoDir });
+  public push(force?: boolean) {
+    const cmd = `git push --tags ${this.remoteName} ${this.focusedBranch} ${force ? '-f' : ''}`
+    this.Logger.log(cmd)
+    cp.execSync(cmd, { cwd: this.repoDir })
+  }
+
+  public getTags(): string[] {
+    const cmd = `git tag --list`
+    this.Logger.log(cmd)
+    return cp
+      .execSync(cmd, { cwd: this.repoDir })
+      .toString()
+      .split('\n')
+      .filter(i => !!i)
+  }
+
+  /**
+   * 删除tag
+   */
+  public removeTag(name: string, remote: boolean = false) {
+    const cmd = !remote ? `git tag --delete ${name}` : `git push ${this.remoteName} --delete tag ${name}`
+    this.Logger.log(cmd)
+    cp.execSync(cmd, { cwd: this.repoDir })
+  }
+
+  /**
+   * 添加tag
+   */
+  public addTag(name: string, message?: string) {
+    const cmd = message == null ? `git tag ${name}` : `git tag -a ${name} -m "${message}"`
+    this.Logger.log(cmd)
+    cp.execSync(cmd, { cwd: this.repoDir })
+  }
+
+  /**
+   * 添加tag，如果已存在则替换它
+   */
+  public addOrReplaceTag(name: string, message?: string) {
+    const cmd = message == null ? `git tag ${name} -f` : `git tag -a ${name} -m "${message}" -f`
+    this.Logger.log(cmd)
+    cp.execSync(cmd, { cwd: this.repoDir })
   }
 
   private mapStatusDesc(desc: string): FileStatus {
-    return FileStatusMap[desc];
+    return FileStatusMap[desc]
   }
 
   private getFiles(src: string) {
-    const files = fs.readdirSync(src);
+    const files = fs.readdirSync(src)
     return files.map(name => {
-      const absolutePath = path.join(src, name);
-      const content = fs.readFileSync(absolutePath).toString();
+      const absolutePath = path.join(src, name)
+      const content = fs.readFileSync(absolutePath).toString()
       return {
         absolutePath,
         content,
         name,
-      };
-    });
+      }
+    })
   }
 }
--- src/deploy.ts
+++ src/deploy.ts
@@ -1,28 +1,28 @@
 /**
  * 部署程序
  */
-import fs from 'fs-extra';
-import glob from 'glob';
-import inquirer from 'inquirer';
-import childProcess from 'child_process';
-import path from 'path';
-import url from 'url';
-import GitUtils from './GitUtils';
-import { getConfigFilePath, getPkg, getOrCreateWorkDir } from './utils';
-import { Configuration, ConfigurableKeys } from './constants';
+import fs from 'fs-extra'
+import glob from 'glob'
+import inquirer from 'inquirer'
+import childProcess from 'child_process'
+import path from 'path'
+import url from 'url'
+import GitUtils from './GitUtils'
+import { getConfigFilePath, getPkg, getOrCreateWorkDir } from './utils'
+import { Configuration, ConfigurableKeys } from './constants'
 
 function getConfig(): Configuration {
-  const pkg = getPkg();
-  const configPath = getConfigFilePath();
+  const pkg = getPkg()
+  const configPath = getConfigFilePath()
   if (!fs.existsSync(configPath)) {
-    console.error('Call `jm-deploy init` at Node project root before deploy.');
-    process.exit(-1);
+    console.error('Call `jm-deploy init` at Node project root before deploy.')
+    process.exit(-1)
   }
-  const conf = require(configPath) as ConfigurableKeys;
+  const conf = require(configPath) as ConfigurableKeys
 
   if (!fs.existsSync(conf.dist)) {
-    console.error(`${conf.dist} is not found.`);
-    process.exit(-1);
+    console.error(`${conf.dist} is not found.`)
+    process.exit(-1)
   }
 
   return {
@@ -30,7 +30,7 @@ function getConfig(): Configuration {
     version: pkg.version,
     remote: conf.remote,
     dist: conf.dist,
-  };
+  }
 }
 
 function clearDir(path: string) {
@@ -38,18 +38,18 @@ function clearDir(path: string) {
     cwd: path,
     dot: false,
     absolute: true,
-  });
-  items.forEach(item => fs.removeSync(item));
+  })
+  items.forEach(item => fs.removeSync(item))
 }
 
 function copyFiles(src: string, dest: string) {
-  fs.copySync(src, dest);
+  fs.copySync(src, dest)
 }
 
 function updateContent(repoDir: string, contentDir: string) {
-  clearDir(repoDir);
-  copyFiles(contentDir, repoDir);
-  childProcess.execSync('git add .', { cwd: repoDir });
+  clearDir(repoDir)
+  copyFiles(contentDir, repoDir)
+  childProcess.execSync('git add .', { cwd: repoDir })
 }
 
 async function getCommitMessage(repoDir: string) {
@@ -59,33 +59,27 @@ async function getCommitMessage(repoDir: string) {
       name: 'message',
       validate: (v: string) => {
         if (v == null || v.trim() === '') {
-          return '不能为空';
+          return '不能为空'
         }
-        return true;
+        return true
       },
     },
-  ]);
-  return ans.message;
+  ])
+  return ans.message
 }
 
-async function updateTags(repoDir: string, name: string, version: string) {
-  const macthed = version.match(/(\d+)\.(\d+)\.(\d+).*/);
+async function updateTags(repo: GitUtils, name: string, version: string) {
+  const macthed = version.match(/(\d+)\.(\d+)\.(\d+).*/)
   if (macthed == null) {
-    console.error('版本号有误:', version);
-    process.exit(-1);
-    return;
+    console.error('版本号有误:', version)
+    process.exit(-1)
+    return
   }
 
-  const [major, minor, fixed] = macthed.slice(1);
-  const tags = childProcess
-    .execSync(`git tag -l`, { cwd: repoDir })
-    .toString()
-    .split('\n')
-    .filter(i => i != '');
-  const tag = `${name}/${major}.${minor}.${fixed}`;
-  const wideTag = `${name}/${major}.${minor}.x`;
-
-  console.log('更新tags');
+  const [major, minor, fixed] = macthed.slice(1)
+  const tags = repo.getTags()
+  const tag = `${name}/${major}.${minor}.${fixed}`
+  const wideTag = `${name}/${major}.${minor}.x`
 
   if (tags.indexOf(tag) !== -1) {
     // 冲突了
@@ -93,56 +87,35 @@ async function updateTags(repoDir: string, name: string, version: string) {
       type: 'confirm',
       message: `版本: ${version} 已经存在, 是否覆盖?`,
       name: 'ok',
-    });
+    })
 
-    if (ans.ok) {
-      childProcess.execSync(`git tag -d ${tag}`, { cwd: repoDir });
-      childProcess.execSync(`git tag -a ${tag} -m ${tag}`, { cwd: repoDir });
-      childProcess.execSync(`git push origin --delete tag ${tag}`, {
-        cwd: repoDir,
-      });
-    } else {
-      process.exit(0);
+    if (!ans.ok) {
+      process.exit(0)
     }
-  } else {
-    childProcess.execSync(`git tag -a ${tag} -m ${tag}`, { cwd: repoDir });
   }
 
-  // 添加通用tags
-  if (tags.indexOf(wideTag) !== -1) {
-    childProcess.execSync(`git tag -d ${wideTag}`, { cwd: repoDir });
-    childProcess.execSync(`git tag -a ${wideTag} -m ${wideTag}`, {
-      cwd: repoDir,
-    });
-    childProcess.execSync(`git push origin --delete tag ${wideTag}`, {
-      cwd: repoDir,
-    });
-  } else {
-    childProcess.execSync(`git tag -a ${wideTag} -m ${wideTag}`, {
-      cwd: repoDir,
-    });
-  }
+  repo.addOrReplaceTag(tag)
+  repo.addOrReplaceTag(wideTag)
 }
 
 export default async function deploy() {
   // TODO: validate config
   // TODO: 提供commit模板
-  // TODO: 优化tag更新
-  const conf = getConfig();
-  const workdir = getOrCreateWorkDir();
-  const pathname = url.parse(conf.remote).pathname || '/repo';
-  const basename = path.basename(pathname, '.git');
-  const repoDir = path.join(workdir, basename);
+  const conf = getConfig()
+  const workdir = getOrCreateWorkDir()
+  const pathname = url.parse(conf.remote).pathname || '/repo'
+  const basename = path.basename(pathname, '.git')
+  const repoDir = path.join(workdir, basename)
 
-  const repo = new GitUtils(workdir, repoDir, conf.remote, 'origin');
-  repo.initial();
-  repo.initialBranch(conf.name);
-  updateContent(repoDir, conf.dist);
+  const repo = new GitUtils(workdir, repoDir, conf.remote, 'origin')
+  repo.initial()
+  repo.initialBranch(conf.name)
+  updateContent(repoDir, conf.dist)
   if (repo.shouldCommit()) {
-    const message = await getCommitMessage(repoDir);
-    repo.commit('test');
-    await updateTags(repoDir, conf.name, conf.version);
-    repo.push();
-    console.log('发布完成!');
+    const message = await getCommitMessage(repoDir)
+    repo.commit(message)
+    updateTags(repo, conf.name, conf.version)
+    repo.push(true)
+    console.log('发布完成!')
   }
 }
